c5502d2d9e91b5cec4ed4d0f29f932447c0840ee
slioplark <jeailfy9520@outlook.com>
2019-11-10 03:13:59 +0800

    feat: get data from firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,10 @@ export class AppComponent {
 
   title = 'angular-adsense';
 
-  constructor(private db: AngularFirestore) { }
+  constructor(private db: AngularFirestore) {
+    db.collection('/05b092a2-036d-4388-9377-ea0dd0b4dc8a').valueChanges().subscribe(res => {
+      console.log(res);
+    });
+  }
 
 }
